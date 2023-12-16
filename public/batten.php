ff80fc9a008c38aee0a3af67ef1fb3a7354beabe
彪大大 <hityb@qq.com>
2023-12-16 10:37:26 +0800

    优化文章meta显示

--- public/batten.php
+++ public/batten.php
@@ -18,19 +18,14 @@
 <h1 class="joe_detail__title"><?php $this->title() ?></h1>
 <div class="joe_detail__count">
   <div class="joe_detail__count-information">
-    <img width="35" height="35" class="avatar lazyload" src="<?php _getAvatarLazyload(); ?>" data-src="<?php _getAvatarByMail($this->author->mail) ?>" alt="<?php $this->author(); ?>" />
+    <time class="joe_detail__count-created" datetime="<?php $this->date('m/d'); ?>"><?php $this->date('Y/m/d'); ?></time>
     <div class="meta">
-      <div class="author">
-        <a class="link" href="<?php $this->author->permalink(); ?>" title="<?php $this->author(); ?>"><?php $this->author(); ?></a>
-      </div>
       <div class="item">
-        <span class="text"><?php $this->date('Y-m-d'); ?></span>
-        <span class="line">/</span>
         <span class="text"><?php $this->commentsNum('%d'); ?> 评论</span>
         <span class="line">/</span>
         <span class="text" id="Joe_Article_Views"><?php _getViews($this); ?> 阅读</span>
       </div>
     </div>
   </div>
-  <time class="joe_detail__count-created" datetime="<?php $this->date('m/d'); ?>"><?php $this->date('m/d'); ?></time>
+  
 </div>
